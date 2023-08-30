082a6b1dff1bf835433d58a1101a0e2cae843080
EvgeniyVashinko <e.vashinko@gmail.com>
2023-08-31 00:55:14 +0300

    added aut to webApp

--- ProductCatalog.WebApp/Startup.cs
+++ ProductCatalog.WebApp/Startup.cs
@@ -1,21 +1,58 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace ProductCatalog.WebApp
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<IHttpContextAccessor, HttpContextAccessor>();
+
+            services.AddHttpClient("ProductCatalogApi", config =>
+            {
+                config.BaseAddress = new Uri(Configuration["ServiceHosts:ProductCatalogApiHost"]);
+                config.Timeout = new TimeSpan(0, 0, 60);
+                config.DefaultRequestHeaders.Authorization = new("Bearer", new HttpContextAccessor().HttpContext.Request.Cookies["token"].ToString());
+            });
+
+            services.AddHttpClient("Auth", config =>
+            {
+                config.BaseAddress = new Uri(Configuration["ServiceHosts:ProductCatalogApiHost"]);
+                config.Timeout = new TimeSpan(0, 0, 60);
+                config.DefaultRequestHeaders.Clear();
+            });
+
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(options =>
+                {
+                    options.LoginPath = new PathString("/Account/Login");
+                    options.AccessDeniedPath = new PathString("/Account/Login");
+                });
+
+            services.AddMvc()
+                .AddJsonOptions(x =>
+                {
+                    x.JsonSerializerOptions.PropertyNameCaseInsensitive = false;
+                    x.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,14 +63,18 @@ namespace ProductCatalog.WebApp
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseStaticFiles();
+
             app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Account}/{action=Login}/{id?}");
             });
         }
     }
